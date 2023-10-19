74e6a974affdc5ec244469d5d9360c446f9577d7
gaoyanliang <yanliang2025@163.com>
2023-10-19 16:33:05 +0800

    接入华为统一扫码服务，提供扫码能力

--- app/src/main/java/com/example/nsyy/MainActivity.java
+++ app/src/main/java/com/example/nsyy/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.nsyy;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Parcelable;
+import android.text.TextUtils;
 import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.ValueCallback;
 import android.widget.Toast;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.nsyy.alarm.LongRunningService;
+import com.example.nsyy.code_scan.CommonActivity;
+import com.example.nsyy.code_scan.DefinedActivity;
 import com.example.nsyy.service.NsServerService;
 import com.example.nsyy.service.NsyyServerBroadcastReceiver;
 import com.example.nsyy.utils.BlueToothUtil;
@@ -22,10 +37,32 @@ import com.example.nsyy.utils.LocationUtil;
 import com.example.nsyy.utils.NotificationUtil;
 import com.example.nsyy.utils.PermissionUtil;
 
-public class MainActivity extends AppCompatActivity {
+import com.huawei.hms.hmsscankit.ScanUtil;
+import com.huawei.hms.ml.scan.HmsScan;
+import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions;
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    /**
+     * Define requestCode.
+     */
+    public static final int CAMERA_REQ_CODE = 111;
+    public static final int DEFINED_CODE = 222;
+    public static final int BITMAP_CODE = 333;
+    public static final int MULTIPROCESSOR_SYN_CODE = 444;
+    public static final int MULTIPROCESSOR_ASYN_CODE = 555;
+    public static final int DECODE = 1;
+    public static final int GENERATE = 2;
+    private static final int REQUEST_CODE_SCAN_ONE = 0X01;
+    private static final int REQUEST_CODE_DEFINE = 0X0111;
+    private static final int REQUEST_CODE_SCAN_MULTI = 0X011;
+    public static final String DECODE_MODE = "decode_mode";
+    public static final String RESULT = "SCAN_RESULT";
 
     public static final String TAG = "Nsyy";
-    private static final String LOAD_RUL = "http://oa.nsyy.com.cn:6060";
+    //private static final String LOAD_RUL = "http://oa.nsyy.com.cn:6060";
+    // 测试扫码功能
+    private static final String LOAD_RUL = "https://dnswc2-vue-demo.site.laf.dev/";
     private WebView webView;
     private SwipeRefreshLayout swipeRefreshLayout;
 
@@ -78,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
         LocationUtil.getInstance().setContext(this);
         LocationUtil.getInstance().initGPS();
 
-
         // 检查是否开启蓝牙权限 & 初始化
         PermissionUtil.checkBlueToothPermission(this);
         BlueToothUtil.getInstance().init(this);
@@ -106,12 +142,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
         webView = findViewById(R.id.webView);
+
         // Enable Javascript
         WebSettings webSettings = webView.getSettings();
         webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         // 设置 WebView 允许执行 JavaScript 脚本
         webSettings.setJavaScriptEnabled(true);
-        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        // Add the JavaScriptInterface to the WebView
+        webView.addJavascriptInterface(this, "AndroidInterface");
 
         // 确保跳转到另一个网页时仍然在当前 WebView 中显示,而不是调用浏览器打开
         webView.setWebViewClient(new WebViewClient() {
@@ -119,8 +161,59 @@ public class MainActivity extends AppCompatActivity {
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
+
+        // 重写 javascript 的 alert 和 confirm 函数,弹窗界面更美观。
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+                b.setTitle("Alert");
+                b.setMessage(message);
+                b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                });
+                b.setCancelable(false);
+                b.create().show();
+                return true;
+            }
+
+            //设置响应js 的Confirm()函数
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+                AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+                b.setTitle("Confirm");
+                b.setMessage(message);
+                b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                });
+                b.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.cancel();
+                    }
+                });
+                b.create().show();
+                return true;
+            }
+        });
+
         // 加载 南石OA
         webView.loadUrl(LOAD_RUL);
+    }
+
+    @JavascriptInterface
+    public void scanCode(){
+        // 多种模式可选： https://developer.huawei.com/consumer/cn/doc/development/HMSCore-Guides/android-overview-0000001050282308
+        //loadScanKitBtnClick();
+        newViewBtnClick();
+        //multiProcessorSynBtnClick();
+        //multiProcessorAsynBtnClick();
     }
 
     // 接管返回按键的响应
@@ -156,4 +249,185 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(nsyyServerBroadcastReceiver);
         stopService(new Intent(this, NsServerService.class));//停止服务
     }
+
+
+    // 接入华为统一扫码功能：https://developer.huawei.com/consumer/cn/doc/development/HMSCore-Guides/android-dev-process-0000001050043953
+
+    /**
+     * Call the default view.
+     */
+    public void loadScanKitBtnClick() {
+        requestPermission(CAMERA_REQ_CODE, DECODE);
+    }
+
+    /**
+     * Call the customized view.
+     */
+    public void newViewBtnClick() {
+        requestPermission(DEFINED_CODE, DECODE);
+    }
+
+    /**
+     * Call the MultiProcessor API in synchronous mode.
+     */
+    public void multiProcessorSynBtnClick() {
+        requestPermission(MULTIPROCESSOR_SYN_CODE, DECODE);
+    }
+
+    /**
+     * Call the MultiProcessor API in asynchronous mode.
+     */
+    public void multiProcessorAsynBtnClick() {
+        requestPermission(MULTIPROCESSOR_ASYN_CODE, DECODE);
+    }
+
+    /**
+     * Apply for permissions.
+     */
+    private void requestPermission(int requestCode, int mode) {
+        if (mode == DECODE) {
+            decodePermission(requestCode);
+        } else if (mode == GENERATE) {
+            // generatePermission(requestCode);
+        }
+    }
+
+    /**
+     * Apply for permissions.
+     */
+    private void decodePermission(int requestCode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_MEDIA_IMAGES},
+                    requestCode);
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE},
+                    requestCode);
+        }
+    }
+
+    /**
+     * Call back the permission application result. If the permission application is successful, the barcode scanning view will be displayed.
+     * @param requestCode Permission application code.
+     * @param permissions Permission array.
+     * @param grantResults: Permission application result array.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (permissions == null || grantResults == null) {
+            return;
+        }
+
+        if (grantResults.length < 2 || grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        //Default View Mode
+        if (requestCode == CAMERA_REQ_CODE) {
+            ScanUtil.startScan(this, REQUEST_CODE_SCAN_ONE, new HmsScanAnalyzerOptions.Creator().create());
+        }
+
+        //Customized View Mode
+        if (requestCode == DEFINED_CODE) {
+            Intent intent = new Intent(this, DefinedActivity.class);
+            try {
+                this.startActivityForResult(intent, REQUEST_CODE_DEFINE);
+            } catch (Exception e) {
+                System.out.println("未成功打开扫码页面，请检查");
+                e.printStackTrace();
+                // Handle the exception
+            }
+        }
+
+        //Multiprocessor Synchronous Mode
+        if (requestCode == MULTIPROCESSOR_SYN_CODE) {
+            Intent intent = new Intent(this, CommonActivity.class);
+            intent.putExtra(DECODE_MODE, MULTIPROCESSOR_SYN_CODE);
+
+            try {
+                this.startActivityForResult(intent, REQUEST_CODE_SCAN_MULTI);
+            } catch (Exception e) {
+                e.printStackTrace();
+                // Handle the exception
+            }
+        }
+        //Multiprocessor Asynchronous Mode
+        if (requestCode == MULTIPROCESSOR_ASYN_CODE) {
+            Intent intent = new Intent(this, CommonActivity.class);
+            intent.putExtra(DECODE_MODE, MULTIPROCESSOR_ASYN_CODE);
+
+            try {
+                this.startActivityForResult(intent, REQUEST_CODE_SCAN_MULTI);
+            } catch (Exception e) {
+                e.printStackTrace();
+                // Handle the exception
+            }
+        }
+    }
+
+    /**
+     * Event for receiving the activity result.
+     *
+     * @param requestCode Request code.
+     * @param resultCode Result code.
+     * @param data        Result.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK || data == null) {
+            return;
+        }
+        //Default View
+        if (requestCode == REQUEST_CODE_SCAN_ONE) {
+            HmsScan obj = data.getParcelableExtra(ScanUtil.RESULT);
+            if (obj != null) {
+                Toast.makeText(this,obj.originalValue,Toast.LENGTH_SHORT).show();
+            }
+            //MultiProcessor & Bitmap
+        } else if (requestCode == REQUEST_CODE_SCAN_MULTI) {
+            Parcelable[] obj = data.getParcelableArrayExtra(CommonActivity.SCAN_RESULT);
+            if (obj != null && obj.length > 0) {
+                //Get one result.
+                if (obj.length == 1) {
+                    if (obj[0] != null && !TextUtils.isEmpty(((HmsScan) obj[0]).getOriginalValue())) {
+                        Toast.makeText(this,((HmsScan) obj[0]).originalValue,Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this,obj[0].describeContents(),Toast.LENGTH_SHORT).show();
+                }
+            }
+            //Customized View
+        } else if (requestCode == REQUEST_CODE_DEFINE) {
+            HmsScan obj = data.getParcelableExtra(DefinedActivity.SCAN_RESULT);
+            if (obj != null) {
+                String retValue = obj.originalValue;
+                Toast.makeText(this, retValue, Toast.LENGTH_SHORT).show();
+
+                try {
+                    String js = "javascript:receiveScanResult('" + retValue + "')";
+                    System.out.println("开始执行 JS 方法：" + js);
+
+                    webView.evaluateJavascript(js, new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String s) {
+                            //将button显示的文字改成JS返回的字符串
+                            System.out.println("成功接收到扫码返回值：" + s);
+                        }
+                    });
+
+                    //webView.loadUrl("javascript:handleScanResult('" + retValue + "')");
+                } catch (Exception e) {
+                    System.out.println("未成功调用 JS 方法 handleScanResult");
+                    e.printStackTrace();
+                    // Handle the exception
+                }
+            }
+        }
+    }
+
+
 }
