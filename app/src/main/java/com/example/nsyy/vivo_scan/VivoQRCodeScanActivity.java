c7348f14c76bed93ff08145c760d46dcdf820a95
gaoyanliang <yanliang2025@163.com>
2024-08-05 10:03:06 +0800

    扫码支持条形码

--- app/src/main/java/com/example/nsyy/vivo_scan/VivoQRCodeScanActivity.java
+++ app/src/main/java/com/example/nsyy/vivo_scan/VivoQRCodeScanActivity.java
@@ -11,6 +11,7 @@ import com.king.camera.scan.analyze.Analyzer;
 import com.king.zxing.DecodeConfig;
 import com.king.zxing.DecodeFormatManager;
 import com.king.zxing.BarcodeCameraScanActivity;
+import com.king.zxing.analyze.MultiFormatAnalyzer;
 import com.king.zxing.analyze.QRCodeAnalyzer;
 
 import androidx.annotation.NonNull;
@@ -30,13 +31,13 @@ public class VivoQRCodeScanActivity extends BarcodeCameraScanActivity {
     public Analyzer<Result> createAnalyzer() {
         //初始化解码配置
         DecodeConfig decodeConfig = new DecodeConfig();
-        decodeConfig.setHints(DecodeFormatManager.QR_CODE_HINTS)//如果只有识别二维码的需求，这样设置效率会更高，不设置默认为DecodeFormatManager.DEFAULT_HINTS
+        decodeConfig.setHints(DecodeFormatManager.ALL_HINTS)//如果只有识别二维码的需求，这样设置效率会更高，不设置默认为DecodeFormatManager.DEFAULT_HINTS
                 .setFullAreaScan(false)//设置是否全区域识别，默认false
                 .setAreaRectRatio(0.8f)//设置识别区域比例，默认0.8，设置的比例最终会在预览区域裁剪基于此比例的一个矩形进行扫码识别
                 .setAreaRectVerticalOffset(0)//设置识别区域垂直方向偏移量，默认为0，为0表示居中，可以为负数
                 .setAreaRectHorizontalOffset(0);//设置识别区域水平方向偏移量，默认为0，为0表示居中，可以为负数
         // BarcodeCameraScanActivity默认使用的MultiFormatAnalyzer，这里可以改为使用QRCodeAnalyzer
-        return new QRCodeAnalyzer(decodeConfig);
+        return new MultiFormatAnalyzer(decodeConfig);
     }
 
     /**
